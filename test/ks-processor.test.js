f64360a675408b5b29428889c3cc240ea225a0bc
tan-tan-kanarek <Jonathan.Kanarek@Kaltura.com>
2018-03-07 14:23:24 +0200

    Handle ks priviledges

--- test/ks-processor.test.js
+++ test/ks-processor.test.js
@@ -1,16 +1,12 @@
-const expect = require("chai").expect;
+const expect = require('chai').expect;
 const kaltura = require('kaltura-ott-client');
 const KsProcessor = require('../lib/processors/ks');
 
-describe("ks-processor", () => {
-    describe("crack", () => {
+describe('ks-processor', () => {
+    describe('crack-partner-id', () => {
     	let ks = 'djJ8MjAzfMvEqu4HezrDJfIF_726hVYCvtU0OH_nWAQd4e3kwtfHj7t2-7UPLXPEYwCnE7HOLS8EgBiM8SRqzDPmwZdYrBrZzrdLtGpOqvJJbhHrfajYG2VCjS-t6nx6I59bQozT81479NcOZGc9vOsJHuCDMpI0r1PEzrYOgkq--xiInHW3HZOCLrSXS8c8mWvHngSZQg==';
 
-		
-        let clientConfig = new kaltura.Configuration();
-        let client = new kaltura.Client(clientConfig);
 		let ksProcessor = new KsProcessor({
-			client: client,
 			secrets: {
 				203: '12345'
 			}
@@ -22,5 +18,23 @@ describe("ks-processor", () => {
 			expect(details.partnerId).to.equal(203);
 			done();
         });
-    });
+	});
+	
+    describe('crack-session-id', () => {
+    	let ks = 'djJ8MjAzfIAd3BGyQiWiTNR2c8yTjIIVE6-JUV2g9vsGOf1i0-QyA7K3yAlxzfYC1UJ6cE54AmgH5SQwHAu_MBKSj4VFLWybRXHL-UP1R86khXn6PKlf6Z-uftT0-HDb4PH1X83x9sgPs5FER91uuAhvJ3Lw3L3HZRvOY9iH2nQ40RUPSZ6_U_sR6O9RhkrRtfEfdxv-JtDpcqHf_nYVVcH_xE2oLTxMQ0kt4F42EapKPgZfDtmQfgDvqwtn_UOiUq7K8kxI6g==';
+
+		let ksProcessor = new KsProcessor({
+			secrets: {
+				203: '12345'
+			}
+		});
+
+    	it('cracked', (done) => {
+			
+			let details = ksProcessor.crackKs(ks)
+			expect(details.sessionid).to.equal('efbf789cb6384739b82769e17b128fee');
+			done();
+        });
+	});
+	
 });
